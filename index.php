9dd0f386d4f018f488004eaba7054d8f306e3c45
sugichan0116 <32512315+sugichan0116@users.noreply.github.com>
2018-05-01 11:09:28 +0900

    drop

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <?php require('./head.html'); ?>
   <body>
     <?php include ('./menu.html'); ?>
-    <div class="ui container" id="article">
+    <div class="ui main text container" id="article">
       <div class="ui devider"></div>
       <div class="ui piled segment">
         <h2>ホーム</h2>
@@ -16,6 +16,32 @@
         <p>Mail:computer.house.random@gmail.com</p>
         <p>Twitter:@c_h_random</p>
       </div>
+      <div class="ui piled segment">
+        <h2>お問い合わせ</h2>
+        <p>Mail:computer.house.random@gmail.com</p>
+        <p>Twitter:@c_h_random</p>
+      </div>
+      <div class="ui piled segment">
+        <h2>お問い合わせ</h2>
+        <p>Mail:computer.house.random@gmail.com</p>
+        <p>Twitter:@c_h_random</p>
+      </div>
+      <div class="ui piled segment">
+        <h2>お問い合わせ</h2>
+        <p>Mail:computer.house.random@gmail.com</p>
+        <p>Twitter:@c_h_random</p>
+      </div>
+      <div class="ui piled segment">
+        <h2>お問い合わせ</h2>
+        <p>Mail:computer.house.random@gmail.com</p>
+        <p>Twitter:@c_h_random</p>
+      </div>
+      <div class="ui piled segment">
+        <h2>お問い合わせ</h2>
+        <p>Mail:computer.house.random@gmail.com</p>
+        <p>Twitter:@c_h_random</p>
+      </div>
+      
     </div>
   </body>
 </html>
--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 $(function() {
-  $('#article').find('.segment').addClass('transition hidden').transition('zoom');
+  // selector cache
+  var
+    $dropdownItem = $('.main.container .menu .dropdown .item'),
+    $menuItem     = $('.main.container .menu .item').not($dropdownItem),
+    $dropdown     = $('.main.container .menu .ui.dropdown'),
+    // alias
+    handler = {
+
+      activate: function() {
+        if(!$(this).hasClass('dropdown browse')) {
+          $(this)
+            .addClass('active')
+            .closest('.ui.menu')
+            .find('.item')
+              .not($(this))
+              .removeClass('active')
+          ;
+        }
+      }
+
+    }
+  ;
+
+  $('#article')
+    .find('.segment')
+      .addClass('transition hidden')
+      .transition('zoom')
+  ;
+  $('.ui.dropdown')
+    .dropdown()
+  ;
+
+  $menuItem
+    .on('click', handler.activate)
+  ;
 });
